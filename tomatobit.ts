e66485b95ecbd51859d2d063a04113180b7b91f8
frankieonline <frankie.wwl@gmail.com>
2019-10-06 23:21:13 +0800

    Add files via upload

--- tomatobit.ts
+++ tomatobit.ts
@@ -21,6 +21,15 @@ enum NeoPixelKnownColors {
     Black = 0x000000
 }
 
+enum LCD_AddressType {
+    //% block="Auto Recognition (0)"
+    auto = 0,
+    //% block="PCF8574 (39)"
+    PCF8574 = 39,
+    //% block="PCF8574A (63)"
+    PCF8574A = 63
+}
+
 enum NeoPixelMode {
     //% block="RGB (GRB format)"
     RGB = 0,
@@ -327,5 +336,90 @@ namespace tomatobit {
     //% weight=799
     export function externalButton(ioPin: DigitalPin): boolean {
         return ((pins.digitalReadPin(ioPin) == 1) ? true : false);
+    }
+
+    let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574
+    let BK: number      // backlight control
+    let RS: number      // command/data
+
+    // set LCD reg
+    function setreg(d: number) {
+        pins.i2cWriteNumber(i2cAddr, d, NumberFormat.Int8LE);
+        basic.pause(1);
+    }
+
+    // send data to I2C bus
+    function set(d: number) {
+        d = d & 0xF0;
+        d = d + BK + RS;
+        setreg(d);
+        setreg(d + 4);
+        setreg(d);
+    }
+
+    // send command
+    function cmd(d: number) {
+        RS = 0;
+        set(d);
+        set(d << 4);
+    }
+
+    // send data
+    function dat(d: number) {
+        RS = 1;
+        set(d);
+        set(d << 4);
+    }
+
+    // Auto identify I2C Address
+    function AutoAddr() {
+        let k = true;
+        let addr = 0x20;
+        let d1 = 0, d2 = 0;
+        while (k && (addr < 0x28)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE);
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16;
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE);
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE);
+            if ((d1 == 7) && (d2 == 0)) k = false;
+            else addr += 1;
+        }
+        if (!k) return addr;
+
+        addr = 0x38;
+        while (k && (addr < 0x40)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE);
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16;
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE);
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE);
+            if ((d1 == 7) && (d2 == 0)) k = false;
+            else addr += 1;
+        }
+        if (!k) return addr;
+        else return 0;
+    }
+
+    /** Initialize LCD, set I2C address. According to the chip, there is two address,
+    * PCF8574 is 39, PCF8574A is 63, 0 for auto recognition
+     * @param address is i2c address for LCD
+     */
+    //% blockId="lcdSetAddress" block="Initialize LCD, set I2C address as %addr"
+    //% group="Component & Sensor"
+    //% weight=799
+    export function lcdSetAddress(addr: LCD_AddressType): void {
+        if (addr == 0) i2cAddr = AutoAddr();
+        else i2cAddr = addr;
+        BK = 8;
+        RS = 0;
+        cmd(0x33);       // set 4bit mode
+        basic.pause(5);
+        set(0x30);
+        basic.pause(5);
+        set(0x20);
+        basic.pause(5);
+        cmd(0x28);       // set mode
+        cmd(0x0C);
+        cmd(0x06);
+        cmd(0x01);       // clear
     }
 }
